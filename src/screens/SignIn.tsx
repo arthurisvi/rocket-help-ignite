a6d2fc4aa6d2a60509c3b9cc435c15b80cd10569
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-07-24 02:31:51 -0300

    feat: capturing user login data

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,11 +1,20 @@
+import { useState } from 'react'
 import { VStack, Heading, Icon, useTheme } from 'native-base' 
 import { Input } from '../components/Input'
 import { Button } from '../components/Button'
 import { Envelope, Key } from 'phosphor-react-native'
 import Logo from '../assets/logo_primary.svg';
 
+
 export function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword ] = useState('');
+  
   const { colors } = useTheme();
+
+   function handleSignIn() {
+     console.log(email, password);
+   }
 
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
@@ -15,19 +24,22 @@ export function SignIn() {
       </Heading>
 
       <Input
-        placeholder="E-mail"
         mb={4}
+        placeholder="E-mail"
         InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
+        onChangeText={setEmail}
       />
       <Input
+        mb={8}
         placeholder="Senha"
         InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
         secureTextEntry
-        mb={8}
+        onChangeText={setPassword}
       />
       <Button
         title="Entrar"
-        w="full"  
+        w="full"
+        onPress={handleSignIn}
       />
     </VStack>
   );  
